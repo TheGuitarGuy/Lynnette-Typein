0fe4a3d45a6dcf8bd8e5f4c98e32f4f0fa988617
Kennion Gubler <gublerkennion@gmail.com>
2023-06-29 16:51:38 -0400

    redesigned tutor interface + keyboard

--- HTML/Assets/Lynnette-animations-dynamic.js
+++ HTML/Assets/Lynnette-animations-dynamic.js
@@ -1,28 +1,32 @@
-let currentRow = 0
+let currentRow = 0;
 
 function showNextRow() {
-  console.log('showNextRow', currentRow)
+  console.log('showNextRow', currentRow);
   let panel = document.getElementById('middlePanel'),
-      row = document.getElementById('row').content.firstElementChild.cloneNode(true),
-      group = row.firstElementChild,
-      input = group.getElementsByClassName('CTATMathInput')[0]
-  console.log(panel, row, group)
-  group.id = `solve${++currentRow}Group`
-  input.id = `solve${currentRow}`
-  group.getElementsByClassName('feedback')[0].id = `solve${currentRow}Feedback`
-  panel.prepend(row)
-  input.focus()
+    row = document
+      .getElementById('row')
+      .content.firstElementChild.cloneNode(true),
+    group = row.firstElementChild,
+    input = group.getElementsByClassName('CTATMathInput')[0];
+  console.log(panel, row, group);
+  group.id = `solve${++currentRow}Group`;
+  input.id = `solve${currentRow}`;
+  group.getElementsByClassName('feedback')[0].id = `solve${currentRow}Feedback`;
+  panel.prepend(row);
+  input.focus();
 }
 
 tutorInitializer.then(() => {
-  updateDone()
-  addFeedbackListener()
-  addHintWindowListener()
-  addSkillListener()
-})
+  updateDone();
+  addFeedbackListener();
+  addHintWindowListener();
+  addSkillListener();
+});
 
 function updateDone() {
-  document.getElementById('doneButton').getElementsByClassName('CTAT-done-button--text')[0].innerHTML = 'Finish Problem'
+  document
+    .getElementById('doneButton')
+    .getElementsByClassName('CTAT-done-button--text')[0].innerHTML = 'Submit';
 }
 
 function addFeedbackListener() {
@@ -30,108 +34,153 @@ function addFeedbackListener() {
     processCommShellEvent: function (event, message) {
       if (event == 'AssociatedRules' && message) {
         let indicator = message.getIndicator(),
-            sai = message.getSAI(),
-            selection = (sai ? sai.getSelection() : '_noSuchComponent_'),
-            selectionElement = document.getElementById(selection)
-        if (selectionElement && selectionElement.classList.contains('CTATMathInput'))
-          window.setTimeout(delayIndicator, 700, selection, indicator)
+          sai = message.getSAI(),
+          selection = sai ? sai.getSelection() : '_noSuchComponent_',
+          selectionElement = document.getElementById(selection);
+        if (
+          selectionElement &&
+          selectionElement.classList.contains('CTATMathInput')
+        )
+          window.setTimeout(delayIndicator, 700, selection, indicator);
       }
-    }
-  })
+    },
+  });
 }
 
 function delayIndicator(selection, indicator) {
   let indicatorBox = document.getElementById(selection + 'Feedback'),
-      currentIndicatorElement = indicatorBox.firstChild,
-      newIndicatorElement = document.createElement('span')
+    currentIndicatorElement = indicatorBox.firstChild,
+    newIndicatorElement = document.createElement('span');
   if (indicator == 'Hint') {
-    newIndicatorElement.innerHTML = '?'
-    newIndicatorElement.classList.add('cross')
+    newIndicatorElement.innerHTML = '?';
+    newIndicatorElement.classList.add('cross');
   } else if (indicator == 'InCorrect') {
-    newIndicatorElement.innerHTML = '&#10005'
-    newIndicatorElement.classList.add('cross')
+    newIndicatorElement.innerHTML = '&#10005';
+    newIndicatorElement.classList.add('cross');
   } else if (indicator == 'Correct') {
-    newIndicatorElement.innerHTML = '&#10003'
-    newIndicatorElement.classList.add('tick')
+    newIndicatorElement.innerHTML = '&#10003';
+    newIndicatorElement.classList.add('tick');
   }
-  if (!currentIndicatorElement) indicatorBox.appendChild(newIndicatorElement)
-  else indicatorBox.replaceChild(newIndicatorElement, currentIndicatorElement)
+  if (!currentIndicatorElement) indicatorBox.appendChild(newIndicatorElement);
+  else indicatorBox.replaceChild(newIndicatorElement, currentIndicatorElement);
 }
 
 function addHintWindowListener() {
   CTATCommShell.commShell.addGlobalEventListener({
     processCommShellEvent: function (event, message) {
       if (event == 'AssociatedRules' && message) {
-        let hintWindow = document.getElementById('HintWindow')
-        hintWindow.style.display = message.getProperty('TutorAdvice') ? 'flex' : 'none'
-        hintWindow.scrollIntoView()
+        let hintWindow = document.getElementById('HintWindow');
+        hintWindow.style.display = message.getProperty('TutorAdvice')
+          ? 'flex'
+          : 'none';
+        hintWindow.scrollIntoView();
       }
       if (event == 'InterfaceAction' && message && typeof message == 'object') {
         let sai = message.getSAI(),
-            selection = (sai ? sai.getSelection() : '_noSuchComponent_')
+          selection = sai ? sai.getSelection() : '_noSuchComponent_';
         if (sai.getAction() == 'SetVisible')
-          document.getElementById(selection).style.display = 'flex'
+          document.getElementById(selection).style.display = 'flex';
       }
-    }
-  })
+    },
+  });
 }
 
 CTATExampleTracerSkill.prototype.clone = function () {
-  let clone = new CTATExampleTracerSkill(this.getCategory(), this.getName(), this.getPGuess(), this.getPKnown(),
-                                         this.getPSlip(), this.getPLearn(), this.getHistory(), this.getOpportunityCount())
-  clone.setLabel(this.getLabel())
-  clone.setDescription(this.getDescription())
-  return clone
-}
+  let clone = new CTATExampleTracerSkill(
+    this.getCategory(),
+    this.getName(),
+    this.getPGuess(),
+    this.getPKnown(),
+    this.getPSlip(),
+    this.getPLearn(),
+    this.getHistory(),
+    this.getOpportunityCount()
+  );
+  clone.setLabel(this.getLabel());
+  clone.setDescription(this.getDescription());
+  return clone;
+};
 
 function addSkillListener() {
-  let initialSkills = [], previousSkills = [], currentSkills = null
+  let initialSkills = [],
+    previousSkills = [],
+    currentSkills = null;
   CTATCommShell.commShell.addGlobalEventListener({
     processCommShellEvent: function (event, message) {
       if (event == 'ProblemRestoreEnd') {
-        currentSkills = CTAT.ToolTutor.tutor.getProblemSummary().getSkills()
-        initialSkills = currentSkills.getAllSkills().map((skill) => skill.clone())
-        previousSkills = currentSkills.getAllSkills().map((skill) => skill.clone())
-        setInitialSkills(initialSkills)
+        currentSkills = CTAT.ToolTutor.tutor.getProblemSummary().getSkills();
+        initialSkills = currentSkills
+          .getAllSkills()
+          .map((skill) => skill.clone());
+        previousSkills = currentSkills
+          .getAllSkills()
+          .map((skill) => skill.clone());
+        setInitialSkills(initialSkills);
       } else if (event == 'AssociatedRules' && message) {
-        setInitialSkills(initialSkills)
+        setInitialSkills(initialSkills);
         let skillUpdates = message.getSkillsObject().getSkillSet(),
-            skillLabelElements = document.getElementsByClassName('CTATSkillWindow--label')
+          skillLabelElements = document.getElementsByClassName(
+            'CTATSkillWindow--label'
+          );
         if (skillUpdates.length) {
-          previousSkills.forEach(skill => {
-            let skillUpdate = skillUpdates.find(skillUpdate =>
-                `${skillUpdate.getSkillName()} ${skillUpdate.getCategory()}` == skill.getSkillName()),
-              skillLabelElement = Array.prototype.find.call(skillLabelElements, skillLabelElement =>
-                skillLabelElement.innerHTML == skill.getLabel())
+          previousSkills.forEach((skill) => {
+            let skillUpdate = skillUpdates.find(
+                (skillUpdate) =>
+                  `${skillUpdate.getSkillName()} ${skillUpdate.getCategory()}` ==
+                  skill.getSkillName()
+              ),
+              skillLabelElement = Array.prototype.find.call(
+                skillLabelElements,
+                (skillLabelElement) =>
+                  skillLabelElement.innerHTML == skill.getLabel()
+              );
             if (skillUpdate) {
-              let skillBarElement = skillLabelElement.previousSibling.firstElementChild
-              skillBarElement.style.width = skill.getPKnown() * 100 + '%'
-              skillBarElement.classList.add('transition')
-              window.setTimeout(() => skillBarElement.style.width = skillUpdate.getLevel() * 100 + '%', 0)
-            } else
-              skillLabelElement.parentNode.classList.add('blur')
-          })
+              let skillBarElement =
+                skillLabelElement.previousSibling.firstElementChild;
+              skillBarElement.style.width = skill.getPKnown() * 100 + '%';
+              skillBarElement.classList.add('transition');
+              window.setTimeout(
+                () =>
+                  (skillBarElement.style.width =
+                    skillUpdate.getLevel() * 100 + '%'),
+                0
+              );
+            } else skillLabelElement.parentNode.classList.add('blur');
+          });
           window.setTimeout(() => {
-            Array.prototype.forEach.call(skillLabelElements, skillLabelElement => {
-              skillLabelElement.previousSibling.firstElementChild.classList.remove('transition')
-              skillLabelElement.parentNode.classList.remove('blur')
-            })
-          }, 1000)
-          previousSkills = currentSkills.getAllSkills().map((skill) => skill.clone())
+            Array.prototype.forEach.call(
+              skillLabelElements,
+              (skillLabelElement) => {
+                skillLabelElement.previousSibling.firstElementChild.classList.remove(
+                  'transition'
+                );
+                skillLabelElement.parentNode.classList.remove('blur');
+              }
+            );
+          }, 1000);
+          previousSkills = currentSkills
+            .getAllSkills()
+            .map((skill) => skill.clone());
         }
       }
-    }
-  })
+    },
+  });
 }
 
 function setInitialSkills(skills) {
-  let skillElementLabels = document.getElementsByClassName('CTATSkillWindow--label')
-  skills.forEach(skill => {
+  let skillElementLabels = document.getElementsByClassName(
+    'CTATSkillWindow--label'
+  );
+  skills.forEach((skill) => {
     let label = skill.getLabel(),
-        initialSkillElement = document.createElement('div')
-    initialSkillElement.classList.add('CTATSkillWindow--initial')
-    initialSkillElement.style.width = skill.getPKnown() * 100 + '%'
-    Array.prototype.find.call(skillElementLabels, labelElement => labelElement.innerHTML == label).
-      previousSibling.append(initialSkillElement)
-  })
+      initialSkillElement = document.createElement('div');
+    initialSkillElement.classList.add('CTATSkillWindow--initial');
+    initialSkillElement.style.width = skill.getPKnown() * 100 + '%';
+    Array.prototype.find
+      .call(
+        skillElementLabels,
+        (labelElement) => labelElement.innerHTML == label
+      )
+      .previousSibling.append(initialSkillElement);
+  });
 }
